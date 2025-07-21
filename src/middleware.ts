522f4eb2d5b67901faa8fda2c3e4c00cb7d2254b
Amaan <amaanshaikh.gg@gmail.com>
2025-07-22 00:02:34 +0530

    new: middleware

---
+++ src/middleware.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+const allowedRoutes = ['/', '/demo', '/auth/register', '/auth/login', '/leaderboard', '/about', '/levels'];
+
+export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+
+  if (
+    pathname.startsWith('/_next') ||
+    pathname.startsWith('/favicon.ico') ||
+    pathname.startsWith('/assets') ||
+    pathname.startsWith('/api')
+  ) {
+    return NextResponse.next();
+  }
+
+  if (allowedRoutes.includes(pathname)) {
+    return NextResponse.next();
+  }
+
+  return NextResponse.redirect(new URL('/auth/login', request.url));
+}

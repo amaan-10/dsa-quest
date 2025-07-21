915fa1a7520ec184762f8cb32829de2e60dd305c
Amaan <amaanshaikh.gg@gmail.com>
2025-07-21 17:59:35 +0530

    update

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,18 +1,23 @@
 // types/next-auth.d.ts
-import "next-auth";
+import { DefaultSession, DefaultUser } from "next-auth";
 
 declare module "next-auth" {
   interface Session {
     user: {
-      name?: string | null;
-      email?: string | null;
-      image?: string | null;
-      token?: string | null; 
-    };
+      token?: string;
+    } & DefaultSession["user"];
   }
 
-  interface User {
+  interface User extends DefaultUser {
     id: string;
     email: string;
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    userId?: string;
+    email?: string;
+    customToken?: string;
   }
 }
